54ddcf11c8a4a61c4634ab274beb0b7ed82fcf42
rajatxs <rajat@developar.in>
2021-10-12 11:32:24 +0530

    Use AdminModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { AppController } from './app.controller'
 import { ConfigModule } from '@nestjs/config'
 import { UserModule } from './user/user.module'
 import { AuthModule } from './auth/auth.module'
-import { SocialRefModule } from './social-ref/social-ref.module';
-import { SocialServiceModule } from './social-service/social-service.module';
+import { SocialRefModule } from './social-ref/social-ref.module'
+import { SocialServiceModule } from './social-service/social-service.module'
+import { AdminModule } from './admin/admin.module'
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { SocialServiceModule } from './social-service/social-service.module';
     AuthModule,
     SocialRefModule,
     SocialServiceModule,
+    AdminModule,
   ],
   controllers: [AppController],
 })
