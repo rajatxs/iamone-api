0c8f1211be1dd6d3bc2c5e87f7822b216f20f435
rajatxs <rajat@developar.in>
2021-10-12 23:47:13 +0530

    Update user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,9 @@
-import { Module, forwardRef, MiddlewareConsumer  } from '@nestjs/common'
+import { Module, MiddlewareConsumer } from '@nestjs/common'
 import { UserService } from './user.service'
 import { UserController } from './user.controller'
-import { AuthModule } from '../auth/auth.module'
 import { HttpRequestMiddleware } from '../http-request/http-request.middleware'
 
 @Module({
-  imports: [forwardRef(() => AuthModule)],
   providers: [UserService],
   controllers: [UserController],
   exports: [UserService]
