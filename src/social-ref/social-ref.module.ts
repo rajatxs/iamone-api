b010c8b89b1162e149fb6a5090dbb7d34e61e310
rajatxs <rajat@developar.in>
2021-10-12 23:46:49 +0530

    Update social-ref module

--- src/social-ref/social-ref.module.ts
+++ src/social-ref/social-ref.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common'
 import { SocialServiceModule } from '../social-service/social-service.module'
-import { AuthModule } from '../auth/auth.module'
 import { UserModule } from '../user/user.module'
 import { SocialRefService } from './social-ref.service'
 import { SocialRefController } from './social-ref.controller'
 
 @Module({
-  imports: [AuthModule, SocialServiceModule, UserModule],
+  imports: [SocialServiceModule, UserModule],
   providers: [SocialRefService],
-  controllers: [SocialRefController]
+  controllers: [SocialRefController],
+  exports: [SocialRefService]
 })
 export class SocialRefModule { }
