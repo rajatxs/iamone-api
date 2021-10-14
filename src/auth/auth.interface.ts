a6f71c43e87a8ab9ff80176b1a9694d56de81e98
rajatxs <rajat@developar.in>
2021-10-14 09:24:25 +0530

    Update auth module

--- src/auth/auth.interface.ts
+++ src/auth/auth.interface.ts
@@ -4,20 +4,15 @@ export interface AuthTokenPayload extends JwtPayload {
    id: string | DocId,
    admin?: boolean,
    name?: string,
-   email?: string,
+   email?: string
+}
+
+export type AdminAuthTokenPayload = AuthTokenPayload
+export interface UserAuthTokenPayload extends AuthTokenPayload {
    email_verified?: boolean
 }
 
-export type UserAuthTokenPayload = AuthTokenPayload
-export type AdminAuthTokenPayload = AuthTokenPayload
-
-export interface UserAuthToken extends ImmutableDoc {
+export interface RegisteredAuthTokenResponse {
    accessToken: string,
    refreshToken: string,
-   userId: DocId,
-   httpRequestId?: DocId
 }
-
-export type PartialUserAuthToken = Partial<UserAuthToken>
-
-export type RegisteredAuthTokenResponse = Omit<UserAuthToken, 'userId' | 'httpRequestId'>
