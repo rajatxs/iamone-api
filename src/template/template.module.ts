83617deeadb9abbc1b0cdfec8057746d88dd85c5
Rajat Sharma <rajat@developar.in>
2021-11-20 23:42:07 +0530

    Update template modules

--- src/template/template.module.ts
+++ src/template/template.module.ts
@@ -1,13 +1,30 @@
 import { Module, MiddlewareConsumer } from '@nestjs/common'
 import { TemplateService } from './template.service'
+
+import { ClinkModule } from '../clink/clink.module'
 import { ClinkService } from '../clink/clink.service'
-import { SiteMetaService } from '../clink/site-meta.service'
 import { SocialServiceProvider } from '../social-service/social-service.service'
+import { SocialRefModule } from '../social-ref/social-ref.module'
+import { SocialRefService } from '../social-ref/social-ref.service'
+import { SocialServiceModule } from '../social-service/social-service.module'
+import { UserModule } from '../user/user.module'
+import { UserService } from '../user/user.service'
 import { TemplateMiddleware } from './template.middleware'
 
 @Module({
-  providers: [TemplateService, SocialServiceProvider, ClinkService, SiteMetaService],
-  // imports: [ClinkService]
+  imports: [
+    SocialRefModule, 
+    SocialServiceModule, 
+    ClinkModule,
+    UserModule
+  ],
+  providers: [
+    TemplateService,
+    SocialRefService, 
+    SocialServiceProvider, 
+    ClinkService,
+    UserService
+  ],
 })
 export class TemplateModule {
   configure(consumer: MiddlewareConsumer) {
