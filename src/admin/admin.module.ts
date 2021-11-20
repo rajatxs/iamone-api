fb0c6613ab864315272dc92a75b427866f0086d6
Rajat Sharma <rajat@developar.in>
2021-11-20 23:41:41 +0530

    Update admin module

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common'
+import { Module, forwardRef } from '@nestjs/common'
 import { AdminService } from './admin.service'
 import { AdminController } from './admin.controller'
+import { AuthModule } from '../auth/auth.module'
 
 @Module({
+   imports: [forwardRef(() => AuthModule)],
    providers: [AdminService],
    controllers: [AdminController],
    exports: [AdminService]
