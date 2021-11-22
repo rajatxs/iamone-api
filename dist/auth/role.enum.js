95fdff785eb65a5a713e1693223ad1f001b786ed
Rajat Sharma <rajat@developar.in>
2021-11-23 00:15:29 +0530

    Use production build

--- dist/auth/role.enum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Role = void 0;
-var Role;
-(function (Role) {
-    Role["Anonymous"] = "anonymous";
-    Role["Admin"] = "admin";
-    Role["User"] = "user";
-})(Role = exports.Role || (exports.Role = {}));
