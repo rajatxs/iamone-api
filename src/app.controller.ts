a84572238e891fd530c933b1b8c8dc50c176541f
Rajat Sharma <rajat@developar.in>
2021-12-10 01:00:20 +0530

    Added Home page

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,17 @@
-import { Controller, HttpCode, Get } from '@nestjs/common'
+import { Controller, HttpCode, Res, Get } from '@nestjs/common'
 import { Role } from './auth/role.enum'
 import { Roles } from './auth/role.decorator'
+import { Response } from 'express'
+import * as path from 'path'
 
 @Controller()
 @Roles(Role.Anonymous)
 export class AppController {
+
+  @Get('/')
+  getHomePage(@Res() res: Response) {
+    return res.sendFile(path.join(__dirname, '../public/index.html'))
+  }
 
   @Get('/_/api/test')
   @HttpCode(200)
