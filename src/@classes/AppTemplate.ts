960ac32bd4f5c15f25bbdb8aae83b30b065a6877
Rajat Sharma <rxx175@gmail.com>
2022-01-28 23:25:43 +0530

    Update AppTemplate class

--- src/@classes/AppTemplate.ts
+++ src/@classes/AppTemplate.ts
@@ -2,18 +2,13 @@ import * as handlebars from 'handlebars'
 import { join } from 'path'
 import { readFile } from 'fs'
 
-export interface AppTemplateInitOptions {
-   themeDir: string,
-   templateDir: string
-}
-
 export abstract class AppTemplate<T> {
    protected instance: typeof handlebars
    protected readonly TEMPLATE_FILE_EXTENSION = 'hbs'
    protected readonly THEME_FILE_EXTENSION = 'css'
    protected readonly DEFAULT_LAYOUT = 'default.layout'
 
-   public constructor(protected options: AppTemplateInitOptions) {
+   public constructor(protected rootDir: string) {
       this.instance = handlebars.create()
    }
 
@@ -35,10 +30,6 @@ export abstract class AppTemplate<T> {
       return this.getCode(this.resolveTemplatePath(templateName))
    }
 
-   private getThemeCode(themeName: string): Promise<string> {
-      return this.getCode(this.resolveThemePath(themeName))
-   }
-
    /** Register new helper */
    protected $helper(name: string, fun: handlebars.HelperDelegate) {
       this.instance.registerHelper(name, fun)
@@ -50,26 +41,21 @@ export abstract class AppTemplate<T> {
    }
 
    /** Compiled specified template file */
-   public async $compile(templateName: string, themeName: string, data: T, options?: handlebars.RuntimeOptions): Promise<string> {
+   public async $compile(templateName: string, data: T, options?: handlebars.RuntimeOptions): Promise<string> {
       const template = this.instance.compile(await this.getTemplateCode(templateName))
-      const theme = this.instance.compile(await this.getThemeCode(themeName))
       const layout = this.instance.compile(await this.getLayoutCode())
       const body = template(data, options)
 
-      return layout(Object.assign(data, { body, css: theme }))
+      return layout(Object.assign(data, { body }))
    }
 
    /** Resolve template path */
    protected resolveTemplatePath(templateName: string) {
-      return join(this.options.templateDir, templateName + '.' + this.TEMPLATE_FILE_EXTENSION)
-   }
-
-   protected resolveThemePath(themeName: string) {
-      return join(this.options.themeDir, themeName + '.' + this.THEME_FILE_EXTENSION)
+      return join(this.rootDir, templateName + '.' + this.TEMPLATE_FILE_EXTENSION)
    }
 
    /** Layout path */
    protected get layoutPath() {
-      return join(this.options.templateDir, this.DEFAULT_LAYOUT + '.' + this.TEMPLATE_FILE_EXTENSION)
+      return join(this.rootDir, this.DEFAULT_LAYOUT + '.' + this.TEMPLATE_FILE_EXTENSION)
    }
 }
