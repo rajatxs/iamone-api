2720e0a05628adef77214be37ad593d012d26edd
Rajat Sharma <rxx175@gmail.com>
2022-03-27 23:24:21 +0530

    Remove auth middleware

--- routes/utilityRoutes.js
+++ routes/utilityRoutes.js
@@ -1,13 +1,11 @@
 import { Router } from 'express';
 import { UtilityController } from '../controllers/UtilityController.js';
-import { UserAuthorization } from '../middlewares/auth.js';
 
 const router = Router();
 const utilityController = new UtilityController();
 
 router.get(
    '/image-kit/auth',
-   UserAuthorization,
    utilityController.getImageKitAuthenticationSignature.bind(utilityController)
 );
 
